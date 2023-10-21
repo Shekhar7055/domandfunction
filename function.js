2aa85448fa93616839dd3734cd7a474d079479d6
Shekhar7055 <140903282+Shekhar7055@users.noreply.github.com>
2023-10-21 19:33:59 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+}
+//add(2,3)
+add(55,60)
+
+function check(n){
+    if(n%2==0){
+        console.log("even")
+    }else{
+        console.log("odd")
+    }
+}
+check(11)
+check(2)
+
